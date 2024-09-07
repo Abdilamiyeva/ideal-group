f9711209fedab2ffb91a854c8a74bfa01fe68d6b
Noila <noilaabdilamiyeva6467@gmail.com>
2024-09-07 19:26:09 +0500

    update

--- src/js/files/main.js
+++ src/js/files/main.js
@@ -1,25 +1,14 @@
+const buttons = document.querySelectorAll('.order__accordion--content__next--btn');
+const contents = document.querySelectorAll('.order__accordion--content');
+let currentIndex = -1;
 
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        currentIndex++;
+        if (currentIndex < contents.length) {
+            contents[currentIndex].classList.add('active');
+        }
+    });
+});
 
-
-console.log("hhhhhhhhhhhhe");
-
-// var swiper = new Swiper(".mySwiper", {
-//   loop: true,
-//   spaceBetween: 10,
-//   slidesPerView: 1,
-//   freeMode: true,
-//   watchSlidesProgress: true,
-// });
-
-// var swiper2 = new Swiper(".mySwiper2", {
-//   loop: true,
-//   spaceBetween: 10,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   thumbs: {
-//     swiper: swiper,
-//   },
-// });
 
