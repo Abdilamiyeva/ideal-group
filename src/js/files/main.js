08639d60f15d66560ff768d6af370abbd4d921c2
Noila <noilaabdilamiyeva6467@gmail.com>
2024-09-08 17:25:01 +0500

    finished project

--- src/js/files/main.js
+++ src/js/files/main.js
@@ -1,60 +1,79 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // order accordion
-    const buttons = document.querySelectorAll('.order__accordion__content__next--btn');
-    const contents = document.querySelectorAll('.order__accordion__content');
-    let currentIndex = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  //modal
+  const modal = document.getElementById("modal");
+  const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
+  const closeModalButton = document.getElementById("modal-close-btn");
 
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            currentIndex++;
-            if (currentIndex < contents.length) {
-                contents[currentIndex].classList.add('active');
-            }
-        });
+  if (addToCartButtons && modal) {
+    addToCartButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        console.log("hello");
+        modal.classList.add("active");
+      });
     });
+  }
 
-    // check input 
-    const checkAllBtn = document.querySelector('.basket__all--checkbtn');
-    if (checkAllBtn) {
-        checkAllBtn.addEventListener('change', function() {
-            const allChecked = this.checked;
-            const checkboxes = document.querySelectorAll('.basket__checkbtn');
+  if (closeModalButton && modal) {
+    closeModalButton.addEventListener("click", function () {
+      modal.classList.remove("active");
+    });
+  }
 
-            checkboxes.forEach(checkbox => {
-                checkbox.checked = allChecked;
-            });
-        });
+  window.addEventListener("click", function (e) {
+    if (e.target === modal) {
+      modal.classList.remove("active");
+    }
+  });
+
+  // order accordion
+  const buttons = document.querySelectorAll(
+    ".order__accordion__content__next--btn"
+  );
+  const contents = document.querySelectorAll(".order__accordion__content");
+  let currentIndex = 0;
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      currentIndex++;
+      if (currentIndex < contents.length) {
+        contents[currentIndex].classList.add("active");
+      }
+    });
+  });
+
+  // check input
+  const checkAllBtn = document.querySelector(".basket__all--checkbtn");
+  if (checkAllBtn) {
+    checkAllBtn.addEventListener("change", function () {
+      const allChecked = this.checked;
+      const checkboxes = document.querySelectorAll(".basket__checkbtn");
+
+      checkboxes.forEach((checkbox) => {
+        checkbox.checked = allChecked;
+      });
+    });
+  }
+
+  // counter
+  document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("counter__increment--btn")) {
+      const wrapper = event.target.closest(".counter__wrapper");
+      const input = wrapper.querySelector(".counter__btn--value");
+      input.value = parseInt(input.value) + 1;
     }
 
+    if (event.target.classList.contains("counter__decrement--btn")) {
+      const wrapper = event.target.closest(".counter__wrapper");
+      const input = wrapper.querySelector(".counter__btn--value");
+      input.value = Math.max(0, parseInt(input.value) - 1);
+    }
+  });
 
-
-    // counter
-    document.addEventListener('click', function(event) {
-        
-        if (event.target.classList.contains('counter__increment--btn')) {
-            const wrapper = event.target.closest('.counter__wrapper');
-            const input = wrapper.querySelector('.counter__btn--value'); 
-            input.value = parseInt(input.value) + 1; 
-        }
-
-        if (event.target.classList.contains('counter__decrement--btn')) {
-            const wrapper = event.target.closest('.counter__wrapper');
-            const input = wrapper.querySelector('.counter__btn--value');
-            input.value = Math.max(0, parseInt(input.value) - 1);
-        }
+  // accordion
+  document.querySelectorAll(".item .title").forEach((title) => {
+    title.addEventListener("click", function () {
+      const item = this.parentElement;
+      item.classList.toggle("active");
     });
-
-    // accordion
-    document.querySelectorAll('.item .title').forEach((title) => {
-        title.addEventListener('click', function() {
-        const item = this.parentElement;
-        item.classList.toggle('active');
-        });
-    });
-  
-
+  });
 });
-
-
-
-
