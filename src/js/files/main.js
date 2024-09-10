c8ff12c91e5719445bc7a25dcb4f6bacf541ac9a
Noila <noilaabdilamiyeva6467@gmail.com>
2024-09-10 20:18:10 +0500

    update

--- src/js/files/main.js
+++ src/js/files/main.js
@@ -1,20 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
   
   // order accordion
-  const buttons = document.querySelectorAll(
-    ".order__accordion__content__next--btn"
-  );
-  const contents = document.querySelectorAll(".order__accordion__content");
-  let currentIndex = 0;
+// Заголовки и контент
+const headers = document.querySelectorAll('.order__accordion__header');
+const contents = document.querySelectorAll('.order__accordion__content');
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      currentIndex++;
-      if (currentIndex < contents.length) {
-        contents[currentIndex].classList.add("active");
-      }
-    });
+// Добавляем обработчик на каждый заголовок
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    // Переключаем класс 'active' для отображения или скрытия контента
+    contents[index].classList.toggle('active');
   });
+});
+
+// Код для кнопок 'Далее'
+const nextButtons = document.querySelectorAll(".order__accordion__content__next--btn");
+const prevButtons = document.querySelectorAll(".order__accordion__content__prev--btn");
+let currentIndex = 0;
+
+nextButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (currentIndex < contents.length - 1) {
+      contents[currentIndex].classList.remove("active");
+      currentIndex++;
+      contents[currentIndex].classList.add("active");
+    }
+  });
+});
+
+prevButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      contents[currentIndex].classList.remove("active");
+      currentIndex--;
+      contents[currentIndex].classList.add("active");
+    }
+  });
+});
+
+// =================================
 
   // check input
   const checkAllBtn = document.querySelector(".basket__all--checkbtn");
